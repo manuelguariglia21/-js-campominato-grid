95fc4e08dd177b8e721ef73113186e83e7eceaa7
Manuel Guariglia <manuelguariglia@gmail.com>
2021-11-02 18:24:20 +0100

    fix selector

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const level = document.getElementById('mg-select');
+
+const playBtn = document.getElementById('playBtn');
+
+playBtn.addEventListener("click", function(){ 
+  console.log(level.value);
+ }); 
