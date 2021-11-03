53d8abe20d9a37818367c26267dfd5961cf34867
Manuel Guariglia <manuelguariglia@gmail.com>
2021-11-03 12:06:47 +0100

    reset

--- js/script.js
+++ js/script.js
@@ -16,68 +16,75 @@ playBtn.addEventListener("click", function(){
 
   start.classList.remove('mg-active');
 
-  if(level.value == "Crazy" && crazyCheck === false){
+  if(level.value == "Crazy"){
     console.log('Crazy', level.value);
     //visibilità
     easy.classList.remove('mg-active');
     hard.classList.remove('mg-active');
     crazy.classList.add('mg-active');
+    if(crazyCheck === false){
+      //create sq
+      for(let i = 1; i <= 49; i++){
+        const crazySq = document.createElement('div');
+        crazySq.className = 'mg-sq mg-crazy-sq';
+        crazy.append(crazySq);
+        crazySq.innerHTML = i;
 
-    //create sq
-    for(let i = 1; i <= 49; i++){
-      const crazySq = document.createElement('div');
-      crazySq.className = 'mg-sq mg-crazy-sq';
-      crazy.append(crazySq);
-      crazySq.innerHTML = i;
+        //sfondo blu al click
+        clickBlue(crazySq);
 
-      //sfondo blu al click
-      clickBlue(crazySq);
-
-      //check
-      crazyCheck = true;
+        //check
+        crazyCheck = true;
+      }
     }
+    
   }
-  else if(level.value == "Easy" && easyCheck === false){
+  else if(level.value == "Easy"){
     console.log('Easy', level.value);
     //visibilità
     crazy.classList.remove('mg-active');
     hard.classList.remove('mg-active');
     easy.classList.add('mg-active');
+    if(easyCheck === false){
+      //create sq
+      for(let i = 1; i <= 100; i++){
+        const easySq = document.createElement('div');
+        easySq.className = 'mg-sq mg-easy-sq';
+        easy.append(easySq);
+        easySq.innerHTML = i;
+        
+        //sfondo blu al click
+        clickBlue(easySq);
 
-    //create sq
-    for(let i = 1; i <= 100; i++){
-      const easySq = document.createElement('div');
-      easySq.className = 'mg-sq mg-easy-sq';
-      easy.append(easySq);
-      easySq.innerHTML = i;
-      
-      //sfondo blu al click
-      clickBlue(easySq);
-
-      //check
-      easyCheck = true;
+        //check
+        easyCheck = true;
+      }
     }
+    
   }
-  else if(level.value == "Hard" && hardCheck === false){
+  else if(level.value == "Hard"){
     console.log('Hard', level.value);
     //visibilità
     crazy.classList.remove('mg-active');
     easy.classList.remove('mg-active');
     hard.classList.add('mg-active');
 
-    //create sq
-    for(let i = 1; i <= 81; i++){
-      const hardSq = document.createElement('div');
-      hardSq.className = 'mg-sq mg-hard-sq';
-      hard.append(hardSq);
-      hardSq.innerHTML = i;
+    if(hardCheck === false){
+      //create sq
+      for(let i = 1; i <= 81; i++){
+        const hardSq = document.createElement('div');
+        hardSq.className = 'mg-sq mg-hard-sq';
+        hard.append(hardSq);
+        hardSq.innerHTML = i;
 
-      //sfondo blu al click
-      clickBlue(hardSq);
+        //sfondo blu al click
+        clickBlue(hardSq);
 
-      //check
-      hardCheck = true;
+        //check
+        hardCheck = true;
+      }
     }
+    
   }
 
  });
